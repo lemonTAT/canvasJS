81e571158f78ff61cb1a759ed5445bd8efa0b8c0
tuzi <2926669196@qq.com>
2017-08-02 20:28:01 +0800

    本地提交

--- canvasAPi.js
+++ canvasAPi.js
@@ -1,97 +1,80 @@
 /* 构造绘画工具 */
 /* 定义构造函数 */
-function Draw(obj,setting){
-    this.obj=obj;
-    this.type=setting.type||"stroke";
-    this.color=setting.color||"#000";
-    this.width=setting.width||"1";
+function Draw(obj, setting) {
+    this.obj = obj;
+    this.type = setting.type || "stroke";
+    this.color = setting.color || "#000";
+    this.width = setting.width || "1";
 }
 /* 使用 prototype 属性来向对象添加属性 */
-Draw.prototype={
+Draw.prototype = {
     /* 初始化 */
-    init:function(){
-        this.obj.strokeStyle=this.color;
-        this.obj.fillStyle=this.color;
-        this.obj.lineWidth=this.width;
+    init: function() {
+        this.obj.strokeStyle = this.color;
+        this.obj.fillStyle = this.color;
+        this.obj.lineWidth = this.width;
     },
     /* 矩形 */
-    rect:function(x,y,x1,y1){
+    rect: function(x, y, x1, y1) {
         this.init();
         this.obj.beginPath();
-        this.obj.rect(x,y,x1-x,y1-y);
-        if(this.type=="stroke"){
+        this.obj.rect(x, y, x1 - x, y1 - y);
+        if (this.type == "stroke") {
             this.obj.stroke();
-        }else if(this.type=="fill"){
+        } else if (this.type == "fill") {
             this.obj.fill();
         }
     },
     /* 直线 */
-    line:function(x,y,x1,y1){
+    line: function(x, y, x1, y1) {
         this.init();
         this.obj.beginPath();
-        this.obj.moveTo(x,y);
-        this.obj.lineTo(x1,y1);
+        this.obj.moveTo(x, y);
+        this.obj.lineTo(x1, y1);
         this.obj.stroke();
     },
     /* 圆形 */
-    circle:function(x,y,x1,y1){
+    circle: function(x, y, x1, y1) {
         this.init();
-        var r=Math.sqrt(Math.pow(x-x1,2)+Math.pow(y-y1,2));
+        var r = Math.sqrt(Math.pow(x - x1, 2) + Math.pow(y - y1, 2));
         this.obj.beginPath();
-        this.obj.arc(x,y,r,0,2*Math.PI);
-        if(this.type=="stroke"){
+        this.obj.arc(x, y, r, 0, 2 * Math.PI);
+        if (this.type == "stroke") {
             this.obj.stroke();
-        }else if(this.type=="fill"){
+        } else if (this.type == "fill") {
             this.obj.fill();
         }
-    },
-    /* 多边形 */
-    poly:function(x,y,x1,y1,n){
-        this.init();
-        var obj=this.obj;
-        var r=Math.sqrt(Math.pow(x-x1,2)+Math.pow(y-y1,2));;
-        obj.save();
-        obj.translate(x,y);
-        obj.rotate(Math.PI/2);
-        var nx=r*Math.cos(Math.PI/n);
-        var ny=r*Math.sin(Math.PI/n);
-        obj.beginPath();
-        obj.lineCap="round";
-        obj.moveTo(nx,ny);
-        for(var i=0;i<=n;i++){
-            obj.rotate(Math.PI*2/n);
-            obj.lineTo(nx,-ny);
-        }
-        if(this.type=="stroke"){
-            this.obj.stroke();
-        }else if(this.type=="fill"){
-            this.obj.fill();
-        }
-        obj.restore();
     },
     /* 铅笔 */
-    pen:function(x,y,x1,y1){
+    pen: function(x, y, x1, y1) {
         this.init();
         this.obj.save();
-        this.obj.lineCap="round";
-        this.obj.lineTo(x1,y1);
+        this.obj.lineCap = "round";
+        this.obj.lineTo(x1, y1);
         this.obj.stroke();
         this.obj.restore();
     },
     /* 橡皮 */
-    eraser:function(x,y,x1,y1){
-        this.obj.lineCap="round";
-        this.obj.clearRect(x1-5,y1-5,10,10);
+    eraser: function(x, y, x1, y1) {
+        this.obj.lineCap = "round";
+        this.obj.clearRect(x1 - 5, y1 - 5, 10, 10);
     },
     /* 剪切 */
-    cut:function(x,y,x1,y1){
+    cut: function(x, y, x1, y1) {
         this.init();
         this.obj.save();
-        this.obj.setLineDash([4,2]);
+        this.obj.setLineDash([4, 2]);
         this.obj.beginPath();
-        this.obj.lineWidth=1;
-        this.obj.rect(x,y,x1-x,y1-y);
+        this.obj.lineWidth = 1;
+        this.obj.rect(x, y, x1 - x, y1 - y);
         this.obj.stroke();
         this.obj.restore();
+    },
+    /* 自由绘画 */
+    free: function(x, y) {
+        this.init();
+        this.obj.beginPath();
+        this.obj.rect(x, y, 1, 1);
+        this.obj.fill();
     }
 }
--- index_shape.js
+++ index_shape.js
@@ -1,11 +1,12 @@
 $(function() {
-    screenHeight = document.documentElement.clientHeight,
-        canvasWidth = 1200,
+    var screenWidth = document.documentElement.clientWidth,
+        screenHeight = document.documentElement.clientHeight,
+        canvasWidth = screenWidth - 40,
         canvasHeight = screenHeight - 200, //canvas标签的大小变量
         canvasDiv = document.getElementById('canvasDiv'), //canvas容器
         canvas = document.createElement('canvas'); //创建了元素canvas
     canvas.setAttribute('width', canvasWidth);
     canvas.setAttribute('height', canvasHeight);
     canvas.setAttribute('id', 'canvas'); //设置画布的id、高度和宽度等属性
-    canvasDiv.appendChild(canvas); //
+    canvasDiv.appendChild(canvas); //为canvasDiv增加了子元素canvas
 })
